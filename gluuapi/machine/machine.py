e90d037a6887558443365bd3e031d20d0c08c81f
shouro <sourogts@gmail.com>
2016-03-29 14:35:54 +0600

    made a thin wrapper of docker-machine

--- gluuapi/machine/machine.py
+++ gluuapi/machine/machine.py
@@ -3,11 +3,94 @@
 #
 # All rights reserved.
 
-def create_machine():
-    pass
+import os
+from functools import wraps
+import json
+from ..utils import run
 
-def create_master_machine():
-    pass
+class Machine(object):
 
-def create_slave_machine():
-    pass
+    def config(self, machine_name):
+        conf = run('docker-machine config {}'.format(machine_name))
+        return conf.strip().replace('\n', ' ')
+
+    # this method is only for swarm master
+    def swarm_config(self, machine_name):
+        conf = run('docker-machine config --swarm {}'.format(machine_name))
+        return conf.strip().replace('\n', ' ')
+
+    def create(self, provider=None):
+        pass
+
+    def inspect(self, machine_name):
+        data = run('docker-machine inspect {}'.format(machine_name))
+        return json.loads(data)
+
+    # Expects a list of machine names
+    def ip(self, machine_name=None):
+        machine_name = machine_name or []
+        if machine_name:
+            if isinstance(machine_name, (list, tuple)):
+                template = 'docker-machine ip ' + ' '.join(['{}']*len(machine_name))
+                ip = run(template.format(*machine_name))
+                return ip.strip()
+
+    # Expects a list of machine names
+    def kill(self, machine_name=None):
+        machine_name = machine_name or []
+        if machine_name:
+            if isinstance(machine_name, (list, tuple)):
+                template = 'docker-machine kill ' + ' '.join(['{}']*len(machine_name))
+                run(template.format(*machine_name))
+
+    def ls(self):
+        pass
+
+    def provision(self):
+        pass
+
+    def regenerate_certs(self):
+        pass
+
+    def restart(self, machine_name):
+        run('docker-machine restart {}'.format(machine_name))
+
+    def rm(self, machine_name=None):
+        machine_name = machine_name or []
+        if machine_name:
+            if isinstance(machine_name, (list, tuple)):
+                template = 'docker-machine rm -y ' + ' '.join(['{}']*len(machine_name))
+                run(template.format(*machine_name))
+
+    def scp(self):
+        pass
+
+    def status(self, machine_name):
+        status = run('docker-machine status {}'.format(machine_name))
+        return status.strip()
+
+    # Expects a list of machine names
+    def start(self, machine_name=None):
+        machine_name = machine_name or []
+        if machine_name:
+            if isinstance(machine_name, (list, tuple)):
+                template = 'docker-machine start ' + ' '.join(['{}']*len(machine_name))
+                run(template.format(*machine_name))
+
+    # Expects a list of machine names
+    def stop(self, machine_name=None):
+        machine_name = machine_name or []
+        if machine_name:
+            if isinstance(machine_name, (list, tuple)):
+                template = 'docker-machine stop ' + ' '.join(['{}']*len(machine_name))
+                run(template.format(*machine_name))
+
+    def upgrade(self):
+        pass
+
+    def url(self, machine_name):
+        url = run('docker-machine url {}'.format(machine_name))
+        return url.strip()
+
+    def version(self):
+        return run('docker-machine version')
--- run.py
+++ run.py
@@ -1,21 +1,8 @@
-'''
-from .app import create_app
-from crochet import setup as crochet_setup
-
-def run_app(app, use_reloader=True):
-    crochet_setup()
-    app.run(
-        host=app.config["HOST"],
-        port=int(app.config["PORT"]),
-        use_reloader=use_reloader,
-    )
-
-if __name__ == '__main__':
-    app = create_app()
-    run_app(app)
-'''
-
 from gluuapi.cli import runserver
+#from gluuapi.machine import Machine
 
 if __name__ == "__main__":
     runserver()
+    #m = Machine()
+    #print m.config('keystore')
+    #print m.swarm_config('master')
